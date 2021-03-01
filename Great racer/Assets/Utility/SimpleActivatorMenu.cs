7f4ffaf0cd2cf75456a441f81db26e90c88cff9e
Dmitry <l.dimka@gmail.com>
2021-03-01 23:05:05 +0300

    game

---
+++ Great racer/Assets/Utility/SimpleActivatorMenu.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace UnityStandardAssets.Utility
+{
+    public class SimpleActivatorMenu : MonoBehaviour
+    {
+        // An incredibly simple menu which, when given references
+        // to gameobjects in the scene
+
+    }
+}

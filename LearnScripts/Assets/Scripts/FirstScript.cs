abd34c14c658c9e64edcd4a3626d7677f9c47a9e
Dmitry <l.dimka@gmail.com>
2021-03-02 23:00:17 +0300

    main update

---
+++ LearnScripts/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    public GameObject Cube;
+    private
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hVal = Input.GetAxis("Horizontal");
+        float vVal = Input.GetAxis("Vertical");
+
+        float mxVal = Input.GetAxis("Mouse X");
+        float myVal = Input.GetAxis("Mouse Y");
+
+        if (hVal != 0)
+        {
+            Cube.transform.Translate(hVal, vVal, 0f);
+        }
+
+        if (vVal != 0)
+        {
+            Cube.transform.Translate(hVal, vVal, 0f);
+        }
+
+        if (mxVal != 0)
+        {
+            Cube.transform.Rotate(0f, mxVal, 0);
+        }
+
+        if (myVal != 0)
+        {
+            transform.Translate(myVal, 0f, 0f);
+        }
+    }
+}
---
+++ LearnScripts/Assets/Scripts/ImportantFunctions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImportantFunctions : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i=1; i <= 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                print($"Это число {i} кратно 3");
+            }else if (i % 5 == 0)
+            {
+                print($"Это число {i} кратно 5");
+            }
+            else
+            {
+                print("Программирование это шик и блеск!");
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

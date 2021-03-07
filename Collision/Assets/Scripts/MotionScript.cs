ef97d3aa183c79769210391b4476bb9caecbfb5f
Dmitry <l.dimka@gmail.com>
2021-03-07 21:17:58 +0300

    segundo

---
+++ Collision/Assets/Scripts/MotionScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xPositio = Input.GetAxis("Horizontal")/2;
+        float yPosition = Input.GetAxis("Vertical")/2;
+        transform.Translate(xPositio, yPosition, 0);
+    }
+}
---
+++ Collision/Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(gameObject.name + " Enter!");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(gameObject.name + " Stay!");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        print(gameObject.name + " Exit!");
+    }
+}

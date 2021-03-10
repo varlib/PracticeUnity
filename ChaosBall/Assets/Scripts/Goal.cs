77e64bdf10d03d0d2f5c864641c6d2dd2c09bf52
Dmitry <l.dimka@gmail.com>
2021-03-10 22:58:34 +0300

    Chaos ball

---
+++ ChaosBall/Assets/Scripts/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    public bool isSolved = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider collider)
+    {
+        GameObject collidedWith = collider.gameObject;
+        if (collidedWith.tag == gameObject.tag)
+        {
+            isSolved = true;
+            GetComponent<Light>().enabled = false;
+            Destroy(collidedWith);
+        }
+    }
+}

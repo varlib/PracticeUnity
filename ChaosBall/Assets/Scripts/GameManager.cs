ebb443c54ac3691c1e40030c7d9d96ea7e666339
Dmitry <l.dimka@gmail.com>
2021-03-11 21:09:26 +0300

    main

---
+++ ChaosBall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Goal blue, green, orange, red;
+    private bool isGameOver=true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGameOver = blue.isSolved && green.isSolved && red.isSolved && orange.isSolved;
+
+    }
+
+    private void OnGUI()
+    {
+        if (isGameOver)
+        {
+            Rect rect = new Rect(Screen.width / 2 - 100, Screen.height / 2 - 50, 200, 75);
+            GUI.Box(rect, "Конец игры");
+
+            Rect rect2 = new Rect(Screen.width / 2 - 30, Screen.height / 2 - 25, 60, 50);
+            GUI.Label(rect2, "Хорошая работа!");
+        }
+    }
+}
